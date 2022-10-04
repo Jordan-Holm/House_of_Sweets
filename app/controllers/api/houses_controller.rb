100e4d0f332d3ee8df0c3baff586741f5ea5cac3
Eric Perkins <ericdperkins7@gmail.com>
2022-10-03 18:37:17 -0600

    made house controller

---
+++ app/controllers/api/houses_controller.rb
@@ -0,0 +1,43 @@
+class Api::HousesController < ApplicationController
+  before_action :set_house, only: [:show, :update, :destroy]
+
+  def index
+    render json: House.all 
+  end
+
+  def show
+    render json: @house
+  end
+
+  def create
+    @house = House.new(house_params)
+    if @house.save 
+      render json: @house
+    else
+      render json: { errors: @house.error }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @house.update(house_params)
+      render json: @house
+    else 
+      render json: { errors: @house.error }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @house.destroy 
+    render json: { message: 'House deleted'}
+  end
+
+  private 
+
+    def house_params
+      params.require(:house).permit(:house_name, :address, :avg_candy, :avg_scary )
+    end
+
+    def set_house
+      @house = House.find(params[:id])
+    end
+end
