b84c072a9af4caa3cd32ae02b75522d149fc0250
Hector Gonzalez <hectorgz25@icloud.com>
2022-10-26 19:08:56 -0600

    finish styling register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,15 +4,17 @@ import Flash from "../shared/Flash";
 import { propTypes } from "react-bootstrap/esm/Image";
 import { Container, Row, Col, Form, Button, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ghostImg from './login/White & Orange Simple Hand Drawn Ghost Character Halloween Logo (1).png'
 
 
-import { imageContainer, imageObject, imageTextRow, registerBody,
+import { imageContainer, imageObject, imageTextRowRegister, registerBody,
     registerShow, registerForm, registerName, registerOrigin, registerEmail, registerPassword, agreementPolicy,
     registerButton, login 
-} from "../../shared/Style";
+} from "../shared/Style";
+
 
 const Register = ({ handleRegister, errors, setErrors }) => {
-    const [user, setUser] = useState({ email: '', password: '' })
+    const [user, setUser] = useState({name: '', username: '', email: '', password: '' })
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -22,19 +24,19 @@ const Register = ({ handleRegister, errors, setErrors }) => {
     return (
         <>
             <Row>
-                {/* <Col as={imageContainer}>
+                <Col>
                     <Row as={imageObject}>
                         <Image 
                             src={ghostImg}
                             alt="Ghost Image"
                         />
                     </Row>
-                    <Row as={imageTextRow}>
+                    <Row as={imageTextRowRegister}>
                         <h1>
                             Find the sweetest home in the neighborhood
                         </h1>
                     </Row>
-                </Col> */}
+                </Col>
                 <Col as={registerBody}>
                     <Row as={registerShow}>
                         <p>
@@ -54,9 +56,12 @@ const Register = ({ handleRegister, errors, setErrors }) => {
                         }
                         <Row>
                             <Col>
-                                <h3>
-                                    Welcom Back to House of Sweets!
-                                </h3>
+                                <h2>
+                                    Create your Free Account 
+                                </h2>
+                                <h5>
+                                    Submit your data for register
+                                </h5>
                             </Col>
                             <Col />
                         </Row>
@@ -68,20 +73,20 @@ const Register = ({ handleRegister, errors, setErrors }) => {
                                         type="name"
                                         name="name"
                                         value={user.name}
-                                        onChange={ (e) => setUser({ ...user, email: e.target.value })}
+                                        onChange={ (e) => setUser({ ...user, name: e.target.value })}
                                         required
                                         placeholder="Name"
                                     />
                                 </Form.Group>
-                                <Form.Group as={registerEmail}>
-                                    <Form.Label>Your Origin</Form.Label>
+                                <Form.Group as={registerOrigin}>
+                                    <Form.Label>Your User Name</Form.Label>
                                     <Form.Control 
-                                        type="origin"
-                                        name="origin"
-                                        value={user.origin}
-                                        onChange={ (e) => setUser({ ...user, email: e.target.value })}
+                                        type="username"
+                                        name="username"
+                                        value={user.user}
+                                        onChange={ (e) => setUser({ ...user, username: e.target.value })}
                                         required
-                                        placeholder="Origin"
+                                        placeholder="User"
                                     />
                                 </Form.Group>
                                 <Form.Group as={registerEmail}>
@@ -142,93 +147,6 @@ const Register = ({ handleRegister, errors, setErrors }) => {
     )
 }
 
-
-
-
-
-
-
-
-
-// const Register = ({ handleRegister, errors, setErrors}) => {
-
-//     const [user, setUser] = useState({ 
-//         email: '',
-//         name: '',
-//         nickname: '',
-//         password: '',
-//         passwordConformation: '' 
-//     })
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault()
-//         if (user.password === user.passwordConformation){
-//             handleRegister(user)
-//         } else {
-//             alert('Password Do Not Match')
-//         }
-//     }
-
-//     return (
-//         <>
-//             { errors ?
-//                 <Flash 
-//                     variant={errors.variant}
-//                     msg={errors.msg}
-//                     setErrors={setErrors}
-//                 />
-//             : null
-//             }
-//             <h1>Create an Account!</h1>
-//             <form onSubmit={handleSubmit}>
-//                 <label>Email</label>
-//                 <input 
-//                     type='email'
-//                     name='email'
-//                     value={user.email}
-//                     onChange={ (e) => setUser({ ...user, email: e.target.value })}
-//                     required
-//                     placeholder='Email'
-//                 />
-//                 <label>Name</label>
-//                 <input 
-//                     name='name'
-//                     value={user.name}
-//                     onChange={ (e) => setUser({ ...user, name: e.target.value })}
-//                     required
-//                     placeholder='Name'
-//                 />
-//                 <label>Username</label>
-//                 <input 
-//                     name='nickname'
-//                     value={user.nickname}
-//                     onChange={ (e) => setUser({ ...user, nickname: e.target.value })}
-//                     required
-//                     placeholder='Username'
-//                 />
-//                 <label>Password</label>
-//                 <input 
-//                     type='password'
-//                     name='password'
-//                     value={user.password}
-//                     onChange={ (e) => setUser({ ...user, password: e.target.value })}
-//                     required
-//                     placeholder='Password'
-//                 />
-//                 <label>Passwrod Conformation</label>
-//                 <input 
-//                     type='password'
-//                     name='passwordConfirmation'
-//                     value={user.passwordConformation}
-//                     onChange={ (e) => setUser({ ...user, passwordConformation: e.target.value })}
-//                     required
-//                     placeholder='Password Confirmation'
-//                 />
-//                 <button type="submit">Submit</button>
-//             </form>
-//         </>
-//     )
-// }
 
 const ConnectedRegister = (props) => (
     <AuthConsumer>
--- client/src/components/shared/Style.js
+++ client/src/components/shared/Style.js
@@ -18,6 +18,11 @@ export const imageTextRow = styled.div `
    color: #000000;
 `
 
+export const imageTextRowRegister = styled.div `
+   margin: -10% 5% 10% 5%;
+   color: #ff7f11;
+`
+
 export const loginBody = styled.div `
    background-color: white;
    color: black;
@@ -45,7 +50,7 @@ export const loginPassword = styled.div `
 export const loginRemember = styled.div `
    padding-bottom: 15%;
 `
-export const loginButton = styled.div `
+export const loginButton = styled.button `
    background-color: blueviolet;
    border-color: blueviolet;
 `
@@ -53,3 +58,71 @@ export const loginButton = styled.div `
 export const register = styled.div `
    text-align: center;
 `
+
+
+
+
+// export const MainP = styled.p`
+//    font-size: 13px;
+// `;
+
+// export const imageContainer = styled.div `
+//    background-color: #cc5803;
+// `
+// export const imageObject = styled.image `
+//    padding: 0%;
+//    margin: -0% 0% 0% 0%;
+//    scale: 85%;
+// `
+
+// export const imageTextRow = styled.div `
+//    margin: -10% 5% 10% 5%;
+//    color: #000000;
+// `
+
+export const registerBody = styled.div `
+   background-color: white;
+   color: black;
+`
+
+export const registerName = styled.div `
+   background-color: white;
+   color: black;
+`
+
+
+export const registerOrigin = styled.div `
+   background-color: white;
+   color: black;
+`
+
+export const registerShow = styled.div `
+   text-align: right;
+   padding-right: 5%;
+`
+
+export const registerForm = styled.div `
+   width: 90%;
+   padding: 10% 10% 10% 10%;
+   border-radius: 2%;
+`
+
+export const registerEmail = styled.div `
+   padding: 5% 0% 5% 0%;
+`
+
+export const registerPassword = styled.div `
+   padding-bottom: 5%;
+`
+
+export const agreementPolicy = styled.div `
+   padding-bottom: 15%;
+`
+export const registerButton = styled.button `
+   background-color: blueviolet;
+   border-color: blueviolet;
+`
+
+export const login = styled.div `
+   text-align: center;
+   `
